a298a2306b3bc901099f215eae85bb24811501bd
Brent Smith <bsmith1310@gmail.com>
2019-12-16 15:34:02 -0600

    dzi extract

--- src/ResizeUtil/index.ts
+++ src/ResizeUtil/index.ts
@@ -6,41 +6,49 @@ import * as sharp from 'sharp';
 
 import MediaSource from '../MediaUtil/models/MediaSource';
 import MediaUrlUtils from '../MediaUrlUtils';
+import FileInfo from '../MediaUtil/models/FileInfo';
 
 class ResizeUtil {
 
   public static GenerateDzi(mediaPath: string, mediaSource: MediaSource): Promise<null> {
     return new Promise((resolve, reject) => {
-      let outputPathParent = MediaUrlUtils.GetCacheFsPathAbsolute(mediaPath, mediaSource.sourceFiles[0].url);
-      try {
-        fs.mkdirSync(outputPathParent, {recursive:true});
-        console.log(`created dir: ${outputPathParent}`);
-      } catch (e) {
-        console.log(`failed to create dir: ${outputPathParent}`);
-      }
-      let outputPath = MediaUrlUtils.GetCachItemFsPathAbsolute(mediaPath, mediaSource.sourceFiles[0].url, 'dzi');
-      try {
-        fs.mkdirSync(outputPath, {recursive:true});
-        console.log(`created dir: ${outputPath}`);
-      } catch (e) {
-        console.log(`failed to create dir: ${outputPath}`);
-      }
-      let filePath = MediaUrlUtils.GetFsFullPathAbsolute(mediaPath, mediaSource.sourceFiles[0].url);
-      sharp(filePath)
-        .tile({
-          size: 512
-        })
-        .toFile(outputPath + '\\dzi', function (err, info) {
-          // output.dzi is the Deep Zoom XML definition
-          // output_files contains 512x512 tiles grouped by zoom level
-          debugger;
-        });
-      console.log('TODO - GenerateDzi');
+      this.GenerateDziForImage(mediaPath, mediaSource.sourceFiles[0]);
     });
+  }
+
+  private static GenerateDziForImage(mediaPath:string, sourceFileInfo:FileInfo) {
+    let outputPathParent = MediaUrlUtils.GetCacheFsPathAbsolute(mediaPath, sourceFileInfo.url);
+    try {
+      fs.mkdirSync(outputPathParent, {recursive:true});
+      console.log(`created dir: ${outputPathParent}`);
+    } catch (e) {
+      console.log(`failed to create dir: ${outputPathParent}`);
+    }
+    let outputPath = MediaUrlUtils.GetCachItemFsPathAbsolute(mediaPath, sourceFileInfo.url, 'dzi');
+    try {
+      fs.mkdirSync(outputPath, {recursive:true});
+      console.log(`created dir: ${outputPath}`);
+    } catch (e) {
+      console.log(`failed to create dir: ${outputPath}`);
+    }
+    let filePath = MediaUrlUtils.GetFsFullPathAbsolute(mediaPath, sourceFileInfo.url);
+    sharp(filePath)
+      .tile({
+        size: 512
+      })
+      .toFile(outputPath + '\\dzi', function (err, info) {
+        // output.dzi is the Deep Zoom XML definition
+        // output_files contains 512x512 tiles grouped by zoom level
+        debugger;
+      });
+    console.log('TODO - GenerateDzi');
   }
 
   public static GenerateThumbnails(mediaPath: string, mediaSource: MediaSource): Promise<null> {
     return new Promise((resolve, reject) => {
+
+      // NOTE: Likely don't need this? DZI should be all that is needed.
+
       console.log('TODO - GenerateThumbnails');
     });
   }
